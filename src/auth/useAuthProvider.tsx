882405107fd6fdbe2c3323c4c7e4c744eb8f20c0
taimurwaheed <taimurwaheed34570@gmail.com>
2025-07-29 15:43:12 +0500

    Handle Auth with User

--- src/auth/useAuthProvider.tsx
+++ src/auth/useAuthProvider.tsx
@@ -1,36 +1,25 @@
-// src/context/auth/useAuthProvider.tsx
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import type { AuthContextType } from '../types/user.types';
 
 export const useAuthProvider = (): AuthContextType => {
     const [user, setUser] = useState(() => {
-        const stored = sessionStorage.getItem('user');
+        const stored = localStorage.getItem('user');
         return stored ? JSON.parse(stored) : null;
     });
 
-    const [hasSignedUp, setHasSignedUp] = useState<boolean>(() => {
-        return localStorage.getItem('hasSignedUp') === 'true';
-    });
-
     const login = (userData: any) => {
-        sessionStorage.setItem('user', JSON.stringify(userData));
+        localStorage.setItem('user', JSON.stringify(userData));
         setUser(userData);
     };
 
     const logout = () => {
-        sessionStorage.removeItem('user');
+        localStorage.removeItem('user');
         setUser(null);
     };
-
-    useEffect(() => {
-        localStorage.setItem('hasSignedUp', String(hasSignedUp));
-    }, [hasSignedUp]);
 
     return {
         user,
         login,
-        logout,
-        hasSignedUp,
-        setHasSignedUp,
+        logout
     };
 };
