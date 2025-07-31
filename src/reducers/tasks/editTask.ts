5b140ad8f253328cb2688f3473a6ab635709e253
taimurwaheed <taimurwaheed34570@gmail.com>
2025-07-31 15:14:34 +0500

    Delete: Reducers

--- src/reducers/tasks/editTask.ts
+++
@@ -1,9 +0,0 @@
-import type { Task } from '../../types/task.types';
-
-export const editTask = (
-    state: Task[],
-    payload: { id: number; text: string }
-): Task[] =>
-    state.map(task =>
-        task.id === payload.id ? { ...task, text: payload.text } : task
-    );
